b3dc2751b243a44735aff867a9fc1d572b2c4c1f
Botahamec <botahamec@outlook.com>
2020-01-21 18:34:44 -0500

    Made tests for removing block comments

--- src/tests.rs
+++ src/tests.rs
@@ -28,3 +28,27 @@ fn tokenize_test() {
 	tokens = vec!["fn", "add1", "{", "var", "temp", ":", "float", "=", "one", "+", "two", "return", "temp", "}"];
 	assert_eq!(tokenize(code), tokens);
 }
+
+#[test]
+fn code_block_test() {
+
+	let mut code = String::from("fn main() {\n\t/* comment */\n\tprint(add(1, 2).to_string())\n}");
+	let mut tokens = vec!["fn", "main", "(", ")", "{", "print", "(", "add", "(", "1", ",", "2", ")", ".", "to_string", "(", ")", ")", "}"];
+	assert_eq!(remove_block_comments(tokenize(code)), tokens);
+
+	code = String::from("result sort(list /*:*/ float): float");
+	tokens = vec!["result", "sort", "(", "list", "float", ")", ":", "float"];
+	assert_eq!(remove_block_comments(tokenize(code)), tokens);
+
+	code = String::from("fn addtwo (one:floa/*t*/, two: float): float {\n\treturn one+two\n}");
+	tokens = vec!["fn", "addtwo", "(", "one", ":", "floa", ",", "two", ":", "float", ")", ":", "float", "{", "return", "one", "+", "two", "}"];
+	assert_eq!(remove_block_comments(tokenize(code)), tokens);
+
+	code = String::from("fn {\n\t //* a comment that has been toggled off\n\treturn one + two\n\t*/\n}");
+	tokens = vec!["fn", "{", "return", "one", "+", "two", "}"];
+	assert_eq!(remove_block_comments(tokenize(code)), tokens);
+
+	code = String::from("fn {\n\t /* a comment that has been toggled on\n\treturn one + two\n\t*/\n}");
+	tokens = vec!["fn", "{", "}"];
+	assert_eq!(remove_block_comments(tokenize(code)), tokens);
+}
