8e39dc34981bef9a86dd45bc2e4f37e92e8df0ad
Botahamec <botahamec@outlook.com>
2020-01-21 14:03:25 -0500

    too much

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 
 extern crate ro_backend;
 
@@ -5,12 +6,13 @@ extern crate ro_backend;
 mod tests;
 
 use std::iter::FromIterator;
+use std::collections::LinkedList;
+
+use ro_backend::{Program, Function, Parameter};
 
 type TokenList = Vec<String>;
 
-const OPERATORS : [&str; 14] = ["(", ":", ")", "{", "}", "=", "+", "-", "*", "/", "=>", "//", "/*", "*/"];
-const KEYWORDS : [&str; 3] = ["fn", "result", "var"];
-
+// tells the tokenizer what to expect next
 #[derive(PartialEq)]
 enum TokenizerMode {
 	LineComment,
@@ -19,30 +21,94 @@ enum TokenizerMode {
 	Normal
 }
 
+#[derive(PartialEq, Clone)]
+enum LexMode {
+	Function,
+	Result
+}
+
+#[derive(Default, PartialEq, Clone)]
+struct LexStack {
+	array: LinkedList<LexMode>
+}
+
+impl LexStack {
+	fn top(self) -> LexMode {
+		self.array.front().unwrap().clone()
+	}
+
+	fn push(&mut self, mode: LexMode) {
+		self.array.push_front(mode);
+	}
+
+	fn pop(&mut self) -> LexMode {
+		self.array.pop_front().unwrap()
+	}
+}
+
+// a list of valid operators
+const OPERATORS : [&str; 17] = ["(", ":", ",", ".", ")", "{", "}", ">", "=", "+", "-", "*", "/", "=>", "//", "/*", "*/"];
+
+// a list of keywords
+const KEYWORDS : [&str; 4] = ["fn", "result", "var", "return"];
+
+// a list of characters which are considered whitespace
+const WHITESPACE : [char; 4] = [' ', '\n', '\t', '\r'];
+
 fn tokenize(code: String) -> TokenList {
-	let mut tokens = TokenList::new();
-	let mut current_token = String::new();
-	let mut mode = TokenizerMode::Normal;
+
+	let mut tokens = TokenList::new(); // what will be returned
+	let mut current_token = String::new(); // the token currently being parsed
+	let mut mode = TokenizerMode::Normal; // the mode that tells the tokenizer what to expect
+
+	// check each character in the String
 	for character in code.chars() {
+
+		// skip over the rest of the line if there's a line comment
 		if mode == TokenizerMode::LineComment {
 			if character == '\n' {
 				mode = TokenizerMode::Normal;
 			}
-		} else if character == ' ' || character == '\n' && current_token != String::new() {
-			tokens.push(current_token.clone());
+
+		// end tokens at whitespace
+		} else if WHITESPACE.contains(&character) {
+			if current_token != String::new() {
+				tokens.push(current_token.clone());
+			}
 			current_token = String::new();
 			mode = TokenizerMode::Normal;
-		} else if OPERATORS.contains(&String::from_iter(vec![character]).as_str()) && current_token != String::new() {
-			tokens.push(current_token.clone());
-			current_token = String::from_iter(vec![character]);
-			if current_token == String::from("//") {
-				mode = TokenizerMode::LineComment;
+
+		// runs if the character is an operator
+		} else if OPERATORS.contains(&String::from_iter(vec![character]).as_str()) {
+
+			// runs if the character combined with the rest of the current token is an operator
+			if OPERATORS.contains(&(current_token.clone() + &String::from_iter(vec![character])).as_str()) {
+				current_token.push(character.clone());
+
+				// ignores the rest of the line if there's a line comment
+				if current_token == String::from("//") {
+					current_token = String::new();
+					mode = TokenizerMode::LineComment;
+
+				// otherwise makes sure the tokenizer expects an operator
+				} else {
+					mode = TokenizerMode::Operator;
+				}
+
+			// runs if it's now two operators
+			} else {
+				tokens.push(current_token.clone());
+				current_token = String::from_iter(vec![character]);
+				mode = TokenizerMode::Operator;
 			}
-			mode = TokenizerMode::Operator;
+
+		// ends the operator token if it was expecting more operators
 		} else if mode == TokenizerMode::Operator {
 			tokens.push(current_token.clone());
 			current_token = String::from_iter(vec![character]);
 			mode = TokenizerMode::Normal;
+
+		// otherwise just add the character to the token
 		} else {
 			current_token.push(character);
 		}
@@ -53,9 +119,60 @@ fn tokenize(code: String) -> TokenList {
 	tokens
 }
 
-// the main function
-/*
-fn parse(code: String) -> ro_backend::program::Program {
+fn remove_block_comments(tokens: TokenList) -> TokenList {
+	let mut in_comment = false;
+	let mut new_list = TokenList::new();
 
+	for token in tokens {
+		if token == String::from("/*") {
+			in_comment = true;
+		} else if token == String::from("*/") {
+			in_comment = false;
+		} else if !in_comment {
+			new_list.push(token);
+		}
+	}
+
+	new_list
 }
-*/
+
+// TODO: remove unwraps
+fn lexer(tokens: TokenList) -> Program {
+
+	let mut new_program = Program::default();
+
+	let mut i = 0;
+	while i < tokens.len() {
+
+		let token = tokens.get(i).unwrap();
+
+		if token == "fn" {
+			let mut new_func = Function::default();
+			new_func.name = String::from(tokens.get(i + 1).unwrap());
+			let mut j = 0;
+			i += 2;
+			while *tokens.get(i + (3 * j) + 1).unwrap() != String::from(")") {
+				let mut new_parameter = Parameter::default();
+				new_parameter.name = tokens.get(i + (3 * j) + 1).unwrap().clone();
+				new_parameter.rotype = tokens.get(i + (3 * j) + 3).unwrap().clone();
+				j += 1
+			}
+			i += 3 * j + 1;
+			if *tokens.get(i + 1).unwrap() == String::from(":") {
+				new_func.return_type = tokens.get(i + 2).unwrap().to_string();
+				i += 2;
+			} else {i += 1;}
+
+			// TODO: add call parsing
+
+			new_program.functions.push(new_func);
+		}
+
+		i += 1;
+	}
+
+	new_program
+}
+
+// the main function
+fn parse(code: String) -> Program {lexer(remove_block_comments(tokenize(code)))}
--- src/tests.rs
+++ src/tests.rs
@@ -2,7 +2,29 @@
 use crate::*;
 
 #[test]
-fn tokenize_empty_function() {
-	let code = String::from("fn main(args: float) {}");
-	assert_eq!(tokenize(code), vec!["fn", "main", "(", "args", ":", "float", ")", "{", "}"]);
+fn tokenize_test() {
+
+	let mut code = String::from("fn main() {\n\t// comment\n\tprint(add(1, 2).to_string())\n}");
+	let mut tokens = vec!["fn", "main", "(", ")", "{", "print", "(", "add", "(", "1", ",", "2", ")", ".", "to_string", "(", ")", ")", "}"];
+	assert_eq!(tokenize(code), tokens);
+
+	code = String::from("result sort(list : float): float");
+	tokens = vec!["result", "sort", "(", "list", ":", "float", ")", ":", "float"];
+	assert_eq!(tokenize(code), tokens);
+
+	code = String::from("fn addtwo (one:float, two: float): float {\n\treturn one+two\n}");
+	tokens = vec!["fn", "addtwo", "(", "one", ":", "float", ",", "two", ":", "float", ")", ":", "float", "{", "return", "one", "+", "two", "}"];
+	assert_eq!(tokenize(code), tokens);
+
+	code = String::from("fn => Add {\n\tvar one'=one+1-1\n\treturn one' + two}");
+	tokens = vec!["fn", "=>", "Add", "{", "var", "one'", "=", "one", "+", "1", "-", "1", "return", "one'", "+", "two", "}"];
+	assert_eq!(tokenize(code), tokens);
+
+	code = String::from("fn {\n\t //* a comment that has been toggled off\n\treturn one + two\n\t*/\n}");
+	tokens = vec!["fn", "{", "return", "one", "+", "two", "*/", "}"];
+	assert_eq!(tokenize(code), tokens);
+
+	code = String::from("fn add1 {\n\t/// docstring\n\tvar temp :float = one +two\n\treturn temp\n}");
+	tokens = vec!["fn", "add1", "{", "var", "temp", ":", "float", "=", "one", "+", "two", "return", "temp", "}"];
+	assert_eq!(tokenize(code), tokens);
 }
