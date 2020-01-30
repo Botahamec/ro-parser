42025f6388d83e156385765ab09661020a092ccd
Botahamec <botahamec@outlook.com>
2020-01-30 13:41:47 -0500

    Refactored the tokenizer somewhat
    All the tokenization happens in a new file

--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,16 @@
 
 extern crate ro_backend;
 
+mod tokenizer;
+
 #[cfg(test)]
 mod tests;
 
-use std::iter::FromIterator;
 use std::collections::LinkedList;
 
-use ro_backend::{Program, Function, Parameter, RoResult};
+use ro_backend::{Program, Function, Parameter};
 
-type TokenList = Vec<String>;
-
-// tells the tokenizer what to expect next
-#[derive(PartialEq)]
-enum TokenizerMode {
-	LineComment,
-	BlockComment,
-	Operator,
-	Normal
-}
+use tokenizer::{tokenize, TokenList};
 
 #[derive(PartialEq, Clone)]
 enum LexMode {
@@ -62,102 +54,6 @@ struct FuncParser {
 struct ResultParser {
 	signature: TokenList,
 	functions: Vec<FuncParser>
-}
-
-// a list of valid operators
-const OPERATORS : [&str; 17] = ["(", ":", ",", ".", ")", "{", "}", ">", "=", "+", "-", "*", "/", "=>", "//", "/*", "*/"];
-
-// a list of keywords
-const KEYWORDS : [&str; 4] = ["fn", "result", "var", "return"];
-
-// a list of characters which are considered whitespace
-const WHITESPACE : [char; 4] = [' ', '\n', '\t', '\r'];
-
-/**
- * Converts Ro code into a list of tokens
- *
- * @author	Mike White
- * @param	code the code as a String
- */
-fn tokenize(code: String) -> TokenList {
-
-	let mut tokens = TokenList::new(); // what will be returned
-	let mut current_token = String::new(); // the token currently being parsed
-	let mut mode = TokenizerMode::Normal; // the mode that tells the tokenizer what to expect
-
-	// check each character in the String
-	for character in code.chars() {
-
-		// skip over the rest of the line if there's a line comment
-		if mode == TokenizerMode::LineComment {
-			if character == '\n' {
-				mode = TokenizerMode::Normal;
-			}
-
-		// end tokens at whitespace
-		} else if WHITESPACE.contains(&character) {
-			if current_token != String::new() {
-				tokens.push(current_token.clone());
-			}
-			current_token = String::new();
-			mode = TokenizerMode::Normal;
-
-		// runs if the character is an operator
-		} else if OPERATORS.contains(&String::from_iter(vec![character]).as_str()) {
-
-			// runs if the character combined with the rest of the current token is an operator
-			if OPERATORS.contains(&(current_token.clone() + &String::from_iter(vec![character])).as_str()) {
-				current_token.push(character.clone());
-
-				// ignores the rest of the line if there's a line comment
-				if current_token == String::from("//") {
-					current_token = String::new();
-					mode = TokenizerMode::LineComment;
-
-				// otherwise makes sure the tokenizer expects an operator
-				} else {
-					mode = TokenizerMode::Operator;
-				}
-
-			// runs if it's now two operators
-			} else {
-				tokens.push(current_token.clone());
-				current_token = String::from_iter(vec![character]);
-				mode = TokenizerMode::Operator;
-			}
-
-		// ends the operator token if it was expecting more operators
-		} else if mode == TokenizerMode::Operator {
-			tokens.push(current_token.clone());
-			current_token = String::from_iter(vec![character]);
-			mode = TokenizerMode::Normal;
-
-		// otherwise just add the character to the token
-		} else {
-			current_token.push(character);
-		}
-	}
-	if current_token != String::new() {
-		tokens.push(current_token.clone());
-	}
-	tokens
-}
-
-fn remove_block_comments(tokens: TokenList) -> TokenList {
-	let mut in_comment = false;
-	let mut new_list = TokenList::new();
-
-	for token in tokens {
-		if token == String::from("/*") {
-			in_comment = true;
-		} else if token == String::from("*/") {
-			in_comment = false;
-		} else if !in_comment {
-			new_list.push(token);
-		}
-	}
-
-	new_list
 }
 
 fn parse_for_fns(tokens: TokenList) -> Vec<FuncParser> {
@@ -285,4 +181,4 @@ fn lexer(tokens: TokenList) -> Program {
 }
 
 // the main function
-fn parse(code: String) -> Program {lexer(remove_block_comments(tokenize(code)))}
+fn parse(code: String) -> Program {lexer(tokenize(code))}
--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,6 @@
 
 use crate::*;
+use tokenizer::*;
 
 #[test]
 fn tokenize_test() {
@@ -7,64 +8,61 @@ fn tokenize_test() {
 	// test a simple function
 	let mut code = String::from("fn main() {\n\t// comment\n\tprint(add(1, 2).to_string())\n}");
 	let mut tokens = vec!["fn", "main", "(", ")", "{", "print", "(", "add", "(", "1", ",", "2", ")", ".", "to_string", "(", ")", ")", "}"];
-	assert_eq!(tokenize(code), tokens);
+	assert_eq!(tokenize_with_block_comments(code), tokens);
 
 	// test a function signature
 	code = String::from("result sort(list : float): float");
 	tokens = vec!["result", "sort", "(", "list", ":", "float", ")", ":", "float"];
-	assert_eq!(tokenize(code), tokens);
+	assert_eq!(tokenize_with_block_comments(code), tokens);
 
 	// test operators when there are no spaces
 	code = String::from("fn addtwo (one:float, two: float): float {\n\treturn one+two\n}");
 	tokens = vec!["fn", "addtwo", "(", "one", ":", "float", ",", "two", ":", "float", ")", ":", "float", "{", "return", "one", "+", "two", "}"];
-	assert_eq!(tokenize(code), tokens);
+	assert_eq!(tokenize_with_block_comments(code), tokens);
 
 	// test operations with no spaces
 	code = String::from("fn => Add {\n\tvar one'=one+1-1\n\treturn one' + two}");
 	tokens = vec!["fn", "=>", "Add", "{", "var", "one'", "=", "one", "+", "1", "-", "1", "return", "one'", "+", "two", "}"];
-	assert_eq!(tokenize(code), tokens);
+	assert_eq!(tokenize_with_block_comments(code), tokens);
 
 	// test toggleable comment blocks
 	code = String::from("fn {\n\t //* a comment that has been toggled off\n\treturn one + two\n\t*/\n}");
 	tokens = vec!["fn", "{", "return", "one", "+", "two", "*/", "}"];
-	assert_eq!(tokenize(code), tokens);
+	assert_eq!(tokenize_with_block_comments(code), tokens);
 
 	// teat weird spacing
 	code = String::from("fn add1 {\n\t/// docstring\n\tvar temp :float = one +two\n\treturn temp\n}");
 	tokens = vec!["fn", "add1", "{", "var", "temp", ":", "float", "=", "one", "+", "two", "return", "temp", "}"];
-	assert_eq!(tokenize(code), tokens);
+	assert_eq!(tokenize_with_block_comments(code), tokens);
 }
 
 #[test]
 fn code_block_test() {
 
-	// closure to shorten certain code
-	let test = |code| remove_block_comments(tokenize(code));
-
 	// test a simple block
 	let mut code = String::from("fn main() {\n\t/* comment */\n\tprint(add(1, 2).to_string())\n}");
 	let mut tokens = vec!["fn", "main", "(", ")", "{", "print", "(", "add", "(", "1", ",", "2", ")", ".", "to_string", "(", ")", ")", "}"];
-	assert_eq!(test(code), tokens);
+	assert_eq!(tokenize(code), tokens);
 
 	// test a block which removes operators
 	code = String::from("result sort(list /*:*/ float): float");
 	tokens = vec!["result", "sort", "(", "list", "float", ")", ":", "float"];
-	assert_eq!(test(code), tokens);
+	assert_eq!(tokenize(code), tokens);
 
 	// test comment blocks at the end of a word
 	code = String::from("fn addtwo (one:floa/*t*/, two: float): float {\n\treturn one+two\n}");
 	tokens = vec!["fn", "addtwo", "(", "one", ":", "floa", ",", "two", ":", "float", ")", ":", "float", "{", "return", "one", "+", "two", "}"];
-	assert_eq!(test(code), tokens);
+	assert_eq!(tokenize(code), tokens);
 
 	// test toggle off blocks
 	code = String::from("fn {\n\t //* a comment that has been toggled off\n\treturn one + two\n\t*/\n}");
 	tokens = vec!["fn", "{", "return", "one", "+", "two", "}"];
-	assert_eq!(test(code), tokens);
+	assert_eq!(tokenize(code), tokens);
 
 	// test toggled on blocks
 	code = String::from("fn {\n\t /* a comment that has been toggled on\n\treturn one + two\n\t*/\n}");
 	tokens = vec!["fn", "{", "}"];
-	assert_eq!(test(code), tokens);
+	assert_eq!(tokenize(code), tokens);
 }
 
 #[test]
